59d6f4c3baa4c1521a5b6e6ed86ede5f81a6fba1
Paulos Tadesse <paulosta952@gmail.com>
2022-03-28 13:54:29 +0300

    :rocket: database structure setup completed

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use App\Models\Click;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Listing extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function clicks()
+    {
+        return $this->hasMany(Click::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
